a6fef1b37f0a603d5d9287a87bc89ad2ee138d5a
tsingson <tsingson@gmail.com>
2017-05-13 00:10:27 +0800

    keep act

--- src/main/java/com/thinkgem/jeesite/common/persistence/ActEntity.java
+++ src/main/java/com/thinkgem/jeesite/common/persistence/ActEntity.java
@@ -10,48 +10,51 @@ import com.thinkgem.jeesite.modules.act.entity.Act;
 
 /**
  * Activiti Entity类
+ *
  * @author ThinkGem
  * @version 2013-05-28
  */
 public abstract class ActEntity<T> extends DataEntity<T> implements Serializable {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	protected Act act; 		// 流程任务对象
+    protected Act act;        // 流程任务对象
 
-	public ActEntity() {
-		super();
-	}
-	
-	public ActEntity(String id) {
-		super(id);
-	}
-	
-	@JsonIgnore
-	public Act getAct() {
-		if (act == null){
-			act = new Act();
-		}
-		return act;
-	}
+    public ActEntity() {
+        super();
+    }
 
-	public void setAct(Act act) {
-		this.act = act;
-	}
+    public ActEntity(String id) {
+        super(id);
+    }
 
-	/**
-	 * 获取流程实例ID
-	 * @return
-	 */
-	public String getProcInsId() {
-		return this.getAct().getProcInsId();
-	}
+    @JsonIgnore
+    public Act getAct() {
+        if (act == null) {
+            act = new Act();
+        }
+        return act;
+    }
 
-	/**
-	 * 设置流程实例ID
-	 * @param procInsId
-	 */
-	public void setProcInsId(String procInsId) {
-		this.getAct().setProcInsId(procInsId);
-	}
+    public void setAct(Act act) {
+        this.act = act;
+    }
+
+    /**
+     * 获取流程实例ID
+     *
+     * @return
+     */
+    public String getProcInsId() {
+        return this.getAct().getProcInsId();
+    }
+
+    /**
+     * 设置流程实例ID
+     *
+     * @param procInsId
+     */
+    public void setProcInsId(String procInsId) {
+        this.getAct().setProcInsId(procInsId);
+    }
 }
